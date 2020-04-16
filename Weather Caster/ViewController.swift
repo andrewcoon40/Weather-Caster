8b11aac491ca0b53b02e5a7028c5ef34cf7897e3
Andrew Coon <acoon@bsd220.org>
2020-04-16 14:21:26 -0500

    Initial Commit

---
+++ Weather Caster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather Caster
+//
+//  Created by Andrew Coon on 4/16/20.
+//  Copyright © 2020 Andrew Coon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
